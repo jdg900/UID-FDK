18bc75173d0367abbcfed391e2075c7d8cf4e5b6
jdg900 <jdg900@naver.com>
2021-10-20 17:21:20 +0900

    upload architecture

--- models.py
+++ models.py
@@ -9,37 +9,7 @@ import torchvision.models as models
 from utils import get_fft_feature
 
 
-class Cascading_Block(nn.Module):
-    def __init__(self, 
-                 in_channels, out_channels,
-                 group=1):
-        super(Cascading_Block, self).__init__()
-
-        self.b1 = ResidualBlock(64, 64)
-        self.b2 = ResidualBlock(64, 64)
-        self.b3 = ResidualBlock(64, 64)
-        self.c1 = BasicBlock(64*2, 64, 1, 1, 0)
-        self.c2 = BasicBlock(64*3, 64, 1, 1, 0)
-        self.c3 = BasicBlock(64*4, 64, 1, 1, 0)
-
-    def forward(self, x):
-        c0 = o0 = x
-
-        b1 = self.b1(o0)
-        c1 = torch.cat([c0, b1], dim=1)
-        o1 = self.c1(c1)
-        
-        b2 = self.b2(o1)
-        c2 = torch.cat([c1, b2], dim=1)
-        o2 = self.c2(c2)
-        
-        b3 = self.b3(o2)
-        c3 = torch.cat([c2, b3], dim=1)
-        o3 = self.c3(c3)
-
-        return o3
-        
-
+# We adopt the network in https://github.com/nmhkahn/CARN-pytorch as the Generator_N2C 
 class Generator_N2C(nn.Module):
     def __init__(self):
         super(Generator_N2C, self).__init__()
@@ -83,6 +53,7 @@ class Generator_N2C(nn.Module):
         return out
 
 
+# We adopt smilar UNet-like network in https://github.com/terryoo/AINDNet as the Generator_C2N 
 class Generator_C2N(nn.Module):
     def __init__(self, input_channel, output_channel, middle_channel=64, n_blocks=6):
         super(Generator_C2N, self).__init__()
@@ -131,60 +102,7 @@ class Generator_C2N(nn.Module):
         return out
 
 
-
-class InResBlock(nn.Module):
-    def __init__(self, dim):
-        super(InResBlock, self).__init__()
-
-        self.norm1 = IN(dim)
-        self.relu1 = nn.LeakyReLU(0.2, inplace=True)
-        self.pad1 = nn.ReflectionPad2d(1)
-        self.conv1 = nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=0)
-
-        self.norm2 = IN(dim)
-        self.relu2 = nn.LeakyReLU(0.2, inplace=True)
-        self.pad2 = nn.ReflectionPad2d(1)
-        self.conv2 = nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=0)
-
-    def forward(self, x):
-        out = self.norm1(x)
-        out = self.relu1(out)
-        out = self.pad1(out)
-        out = self.conv1(out)
-
-        out = self.norm2(out)
-        out = self.relu2(out)
-        out = self.pad2(out)
-        out = self.conv2(out)
-
-        out = out + x
-
-        return out
-
-
-class IN(nn.Module):
-    def __init__(self, num_features, eps=1e-5):
-        super(IN, self).__init__()
-
-        self.eps = eps
-
-        self.gamma = nn.Parameter(torch.ones(1, num_features, 1, 1))
-        self.betta = nn.Parameter(torch.zeros(1, num_features, 1, 1))
-
-    def forward(self, x):
-        in_mean, in_var = torch.mean(x, dim=[2, 3], keepdim=True), torch.var(
-            x, dim=[2, 3], keepdim=True
-        )
-
-        out_in = (x - in_mean) / torch.sqrt(in_var + self.eps)
-
-        out = self.gamma.expand(x.shape[0], -1, -1, -1) * out_in + self.betta.expand(
-            x.shape[0], -1, -1, -1
-        )
-
-        return out
-
-
+# We adopt the PatchGAN in https://github.com/phillipi/pix2pix as clean discriminator and texture discriminator
 class Discriminator(nn.Module):
     def __init__(self, input_nc=3, ndf=64, n_layers=3):
         super(Discriminator, self).__init__()
@@ -240,6 +158,7 @@ class Discriminator(nn.Module):
         return self.model(input)
 
 
+# We adopt the Spectral discrimiantor in https://github.com/cyq373/SSD-GAN
 class Spectral_Discriminator(nn.Module):
     def __init__(self):
         super(Spectral_Discriminator, self).__init__()
@@ -250,6 +169,90 @@ class Spectral_Discriminator(nn.Module):
         az_fft_feature[torch.isnan(az_fft_feature)] = 0
         
         return self.linear(az_fft_feature[:,44:])
+
+
+class Cascading_Block(nn.Module):
+    def __init__(self, 
+                 in_channels, out_channels,
+                 group=1):
+        super(Cascading_Block, self).__init__()
+
+        self.b1 = ResidualBlock(64, 64)
+        self.b2 = ResidualBlock(64, 64)
+        self.b3 = ResidualBlock(64, 64)
+        self.c1 = BasicBlock(64*2, 64, 1, 1, 0)
+        self.c2 = BasicBlock(64*3, 64, 1, 1, 0)
+        self.c3 = BasicBlock(64*4, 64, 1, 1, 0)
+
+    def forward(self, x):
+        c0 = o0 = x
+
+        b1 = self.b1(o0)
+        c1 = torch.cat([c0, b1], dim=1)
+        o1 = self.c1(c1)
+        
+        b2 = self.b2(o1)
+        c2 = torch.cat([c1, b2], dim=1)
+        o2 = self.c2(c2)
+        
+        b3 = self.b3(o2)
+        c3 = torch.cat([c2, b3], dim=1)
+        o3 = self.c3(c3)
+
+        return o3
+
+
+class IN(nn.Module):
+    def __init__(self, num_features, eps=1e-5):
+        super(IN, self).__init__()
+
+        self.eps = eps
+
+        self.gamma = nn.Parameter(torch.ones(1, num_features, 1, 1))
+        self.betta = nn.Parameter(torch.zeros(1, num_features, 1, 1))
+
+    def forward(self, x):
+        in_mean, in_var = torch.mean(x, dim=[2, 3], keepdim=True), torch.var(
+            x, dim=[2, 3], keepdim=True
+        )
+
+        out_in = (x - in_mean) / torch.sqrt(in_var + self.eps)
+
+        out = self.gamma.expand(x.shape[0], -1, -1, -1) * out_in + self.betta.expand(
+            x.shape[0], -1, -1, -1
+        )
+
+        return out
+
+
+class InResBlock(nn.Module):
+    def __init__(self, dim):
+        super(InResBlock, self).__init__()
+
+        self.norm1 = IN(dim)
+        self.relu1 = nn.LeakyReLU(0.2, inplace=True)
+        self.pad1 = nn.ReflectionPad2d(1)
+        self.conv1 = nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=0)
+
+        self.norm2 = IN(dim)
+        self.relu2 = nn.LeakyReLU(0.2, inplace=True)
+        self.pad2 = nn.ReflectionPad2d(1)
+        self.conv2 = nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=0)
+
+    def forward(self, x):
+        out = self.norm1(x)
+        out = self.relu1(out)
+        out = self.pad1(out)
+        out = self.conv1(out)
+
+        out = self.norm2(out)
+        out = self.relu2(out)
+        out = self.pad2(out)
+        out = self.conv2(out)
+
+        out = out + x
+
+        return out
 
 
 class vgg_19(nn.Module):
