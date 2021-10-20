2aceb36df9db8fe42f770d208e1621d287cae126
jdg900 <jdg900@naver.com>
2021-10-20 17:28:24 +0900

    delete file

--- train_awgn_sigma25.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python main.py --exp awgn_sigma25 --patch_size 128 --datarootC ./dataset/train/div2k_trainC.h5 --datarootN ./dataset/train/div2k_trainN.h5 --datarootN_val ./dataset/test/CBSD68 --sigma 25
