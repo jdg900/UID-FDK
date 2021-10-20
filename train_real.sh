2623b3eb4a61653cd2d59eb1ccb61405b36f3e26
jdg900 <jdg900@naver.com>
2021-10-20 17:27:13 +0900

    training scripts

---
+++ train_real.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python main.py --exp sidd  --patch_size 128 --datarootC ./dataset/train/sidd_trainC.h5 --datarootN ./dataset/train/sidd_trainN.h5 --datarootN_val ./dataset/test/SIDD_test --real 1
